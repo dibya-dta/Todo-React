4f9805b259c5627f947f4fd8b214b3f2835d4c96
dibya-dta <dibayatezpur@gmail.com>
2026-09-24 14:55:01 +0530

    Done basic Todo

--- src/App.jsx
+++ src/App.jsx
@@ -4,53 +4,56 @@ import { useEffect, useState } from 'react'
 export default function App() {
   const [input, setInput] = useState("")
   const [task, setTask] = useState([])
+  const [loaded, setLoaded] = useState(false)
 
-
+  //Getting initial values form localstorage
   useEffect(() => {
     const res = JSON.parse(localStorage.getItem('task')) || []
-    // console.log(res)
     setTask(res)
+    setLoaded(true)
   }, [])
 
+
+  //Updating Localstorage along with task
   useEffect(() => {
-    // if(task.length <= 0 ) return
-    // console.log(task)
+    if (!loaded) return
     localStorage.setItem("task", JSON.stringify(task))
   }, [task])
 
-  const handleSubmit = (e)=>{
+
+  //Handling Adding task
+  const handleSubmit = (e) => {
     e.preventDefault()
-    // console.log(e.target[0].value)
-    setInput(e.target[0].value);
-    setTask((prev)=>([...prev,{id:Date.now(),task:input, isRead:false}]))
+    if (!input.trim()) {
+      return
+    }
+    setTask((prev) => ([...prev, { id: Date.now(), task: input, isRead: false }]))
     setInput("")
 
   }
 
-  const handleRead = (id)=>{
-    // e.stopPropagation
-    // console.log(id)
-    // if(e.target.name == "Read"){
-      setTask(task.map((item)=>{
-        return(
-          item.id == id ? {...item, isRead:!item.isRead } : item
-        )
-      }))
-      // localStorage.setItem("task",JSON.stringify([]))
-    // }
 
-    // if(e.target.name == "Delete"){
-    //   setTask(task.map((item)=>{
-    //     return(
-    //       item.id == id ? {...item, isRead:!item.isRead } : item
-    //     )
-    //   }))
-    // }
+  //Handling reading task
+  const handleRead = (id) => {
+    setTask((prev) => (
+      prev.map((item) => (
+        item.id == id ? { ...item, isRead: !item.isRead } : item
+      )
+      )
+    ))
   }
 
-  const handleDelete =(id)=>{
-      setTask(task.filter((item)=>item.id !== id))
-    // }
+
+  //Handling deleting task
+  const handleDelete = (id) => {
+    setTask(prev => prev.filter((item) => item.id !== id))
+  }
+
+
+  //Handling all removal
+  const handleRemove = () => {
+    setTask([])
+    localStorage.removeItem('task')
   }
 
   return (
@@ -58,26 +61,26 @@ export default function App() {
       Hello
       <h1>Todo app</h1>
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder='Enter your task' value={input} onChange={(e)=>setInput(e.target.value)}/>
+        <input type="text" placeholder='Enter your task' value={input} onChange={(e) => setInput(e.target.value)} />
         <button type='submit'>submit</button>
       </form>
-      <button onClick={()=>localStorage.clear()}>Remove all</button>
+      <button onClick={handleRemove} >Remove all</button>
       {
-        task.length >= 0 && 
+        task.length > 0 &&
         <ul className='bg-[#a9f9ff]' >
-         { task.map((item, idx)=>{
-          return(
+          {task.map((item, idx) => {
+            return (
               <li key={item.id} className='bg-[#7cbd7f]'>
                 <span className={` ${item.isRead && "line-through"}`}>{item.task}</span>
                 <button className={`m-2 p-2 border border-black border-solid`}
-                 name='Read' onClick={()=>handleRead(item.id)}>Mark as read</button>
-                <button className={`m-2 p-2 border border-black border-solid`} 
-                name='Delete' onClick={()=>handleDelete(item.id)}>Delete</button>
+                  name='Read' onClick={() => handleRead(item.id)}>Mark as read</button>
+                <button className={`m-2 p-2 border border-black border-solid`}
+                  name='Delete' onClick={() => handleDelete(item.id)}>Delete</button>
               </li>
             )
           })}
         </ul>
-        }
+      }
     </div>
   )
 }
